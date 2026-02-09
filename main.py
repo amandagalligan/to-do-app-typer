91a145bc8f21e438a9cd987c460c8047e7aa8b2f
amandagalligan <ephemerism@gmail.com>
2026-02-09 14:51:23 +0100

    Add goodbye command and usage instructions to main.py

--- main.py
+++ main.py
@@ -8,5 +8,29 @@ def greet(name: str, age: int = 20):
     typer.echo(f"Hellow {name}, you are {age} years old ")
 
 
+@app.command()
+def goodbye(name: str):
+    typer.echo(f"Goodbye, {name}")
+
+
 if __name__ == "__main__":
     app()
+
+"""
+python main.py --help    
+                                                                                                                            
+ Usage: main.py [OPTIONS] COMMAND [ARGS]...                                                                                 
+                                                                                                                            
+╭─ Options ────────────────────────────────────────────────────────────────────────────────────────────────────────────────╮
+│ --install-completion          Install completion for the current shell.                                                  │
+│ --show-completion             Show completion for the current shell, to copy it or customize the installation.           │
+│ --help                        Show this message and exit.                                                                │
+╰──────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────╯
+╭─ Commands ───────────────────────────────────────────────────────────────────────────────────────────────────────────────╮
+│ greet                                                                                                                    │
+│ goodbye                                                                                                                  │
+╰──────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────╯
+
+python main.py goodbye joe
+Goodbye, joe
+"""
