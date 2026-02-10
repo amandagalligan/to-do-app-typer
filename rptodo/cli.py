8dfe8aa485d8c34980fdbcf52656b0579725921f
amandagalligan <ephemerism@gmail.com>
2026-02-10 13:56:14 +0100

    feat(cli): add init command to set up database
    Adds a new `init` command to the command-line interface. This command initializes the to-do application by creating the necessary database and configuration files. It allows users to specify a database path or use the default, and it provides clear feedback on success or failure.

--- rptodo/cli.py
+++ rptodo/cli.py
@@ -2,17 +2,48 @@
 
 from typing import Optional
 import typer
+from pathlib import Path
 from typing_extensions import Annotated
 
-from rptodo import ERRORS, __app_name__, __version__
+from rptodo import ERRORS, __app_name__, __version__, config, database
 
 app = typer.Typer()
+
+
+@app.command()
+def init(
+    db_path: Annotated[
+        str,
+        typer.Option(
+            "--db-path",
+            "-db",
+            prompt="The to-do database location?",
+        ),
+    ] = str(database.DEFAULT_DB_FILE_PATH),
+) -> None:
+    """Initializes the to-do datbase"""
+    app_init_error = config.init_app(db_path)
+    if app_init_error:
+        typer.secho(
+            f'Creating config file failed with "{ERRORS[app_init_error]}"',
+            fg=typer.colors.RED,
+        )
+        raise typer.Exit(1)
+
+    db_init_error = database.init_database(Path(db_path))
+    if db_init_error:
+        typer.secho(
+            f'Creating database failed with "{ERRORS[db_init_error]}"',
+            fg=typer.colors.RED,
+        )
+        raise typer.Exit(1)
+    else:
+        typer.secho(f"The to-do database is {db_path}", fg=typer.colors.GREEN)
+
 
 # typer.echo(...): If the flag was used, it prints the application's
 # name and version to the terminal (e.g., "rptodo v 0.1.0").
 # In short, this is the function that makes a --version option work.
-
-
 def _version_callback(value: bool) -> None:
     if value:
         typer.echo(f"{__app_name__} v {__version__}")
