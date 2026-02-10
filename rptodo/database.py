b25a3d1608f9fc81b74493487487e323d3eb8f55
amandagalligan <ephemerism@gmail.com>
2026-02-10 15:24:29 +0100

    refactor(database): introduce DatabaseHandler for better data management
    This commit refactors the ``database.py`` module to introduce a `DatabaseHandler` class. This change encapsulates the logic for reading and writing to the JSON database, improving the structure and maintainability of the code.
    
    The new `DatabaseHandler` class includes `read_todos` and `write_todos` methods, providing a clearer and more robust interface for database operations. A `DBResponse` named tuple has also been added to ensure consistent and predictable return values.

--- rptodo/database.py
+++ rptodo/database.py
@@ -2,8 +2,52 @@
 
 import configparser
 from pathlib import Path
+import json
+from typing import Any, NamedTuple
 
-from rptodo import DB_WRITE_ERROR, SUCCESS
+from rptodo import DB_WRITE_ERROR, DB_READ_ERROR, SUCCESS, JSON_ERROR
+
+
+class DBResponse(NamedTuple):
+    """Datacontainer to send and receive data from database"""
+
+    todo_list: list[
+        dict[str, Any]
+    ]  # represent list of todos to write and hold values you read from database
+    error: int
+
+
+class DatabaseHandler:
+    def __init__(self, db_path: Path) -> None:
+        self._db_path = db_path
+
+    def read_todos(self) -> DBResponse:
+        """
+        Try to read the todo items from datbase/json file.
+        if it fails return empty list and the ERROR
+        If it succeeds return the list to caller
+        """
+        try:
+            with self._db_path.open("r") as db:
+                try:
+                    return DBResponse(json.load(db), SUCCESS)
+                except json.JSONDecodeError:
+                    return DBResponse([], JSON_ERROR)
+        except OSError:
+            return DBResponse([], DB_READ_ERROR)
+
+    def write_todos(self, todo_list: list[dict[str, Any]]) -> DBResponse:
+        """
+        Try to write todo items to database/json file
+        This method takes a list of to-do's of type list[dict[str, Any]]
+        """
+        try:
+            with self._db_path.open("w") as db:
+                json.dump(todo_list, db, indent=4)
+                return DBResponse(todo_list, SUCCESS)
+        except OSError:
+            return DBResponse(todo_list, DB_WRITE_ERROR)
+
 
 # Define default path to database config file if user doesn't provide one
 DEFAULT_DB_FILE_PATH = Path(__file__).parent.parent.joinpath(".default_todo.json")
